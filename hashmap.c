1a599fb8fb4f18d390d971eae4ec29b2cb88e7f9
Your Name <you@example.com>
2023-09-08 20:52:46 -0300

    new try

--- hashmap.c
+++ hashmap.c
@@ -39,9 +39,17 @@ int is_equal(void* key1, void* key2){
 }
 
 
-void insertMap(HashMap * map, char * key, void * value) {
+void insertMap(HashMap * map, char * key, void * value) 
+{
+    long  posicion = hash(key, map -> capacity);
 
+    while(map -> buckets[posicion] != NULL && map -> buckets[posicion] -> key != NULL);
+    {
+        posicion = (posicion + 1) % map -> capacity;
+    }
 
+    map -> buckets[posicion] = createPair(key, value);
+    
 }
 
 void enlarge(HashMap * map) {
